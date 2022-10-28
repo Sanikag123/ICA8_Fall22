07785e01370eaff5891477597f71edcdad12341e
Sanikag123 <Sgandhe1@asu.edu>
2022-10-27 19:03:46 -0700

    Test Case 2 implemented and passing

--- UrinalTest.java
+++ UrinalTest.java
@@ -18,9 +18,18 @@ class UrinalTest {
 
     @Test
     public void inValidStringInputs() {
+        System.out.println("============ Sanika Yatin Gandhe =========== Test ONE executed");
         String input = "1011";
         boolean actual = false;
         boolean expected = uri.isStringValid(input);
         assertEquals(actual,expected);
+    }
+
+    @Test
+    public void validStringInputs() {
+        System.out.println("============ Sanika Yatin Gandhe =========== Test TWO executed");
+        String input = "101";
+        boolean expected = uri.isStringValid(input);
+        assertTrue(expected);
     }
 }
